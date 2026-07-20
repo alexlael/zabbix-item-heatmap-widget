7031f2d56117b09fb9dc3e7d1644c3ed564432aa
Alex Lael <alex.lael.bsb@gmail.com>
2026-07-20 14:07:39 -0300

    Restore complete widget edit form

--- views/widget.edit.php
+++ views/widget.edit.php
@@ -14,4 +14,33 @@
         new CWidgetFieldMultiSelectItemView($data['fields']['log_itemids'])
     )
     ->addField(
-        new CWidgetField
+        new CWidgetFieldSelectView($data['fields']['aggregation'])
+    )
+    ->addField(
+        new CWidgetFieldSelectView($data['fields']['display_mode'])
+    )
+    ->addField(
+        new CWidgetFieldSelectView($data['fields']['period_weeks'])
+    )
+    ->addField(
+        new CWidgetFieldSelectView($data['fields']['slot_seconds'])
+    )
+    ->addField(
+        new CWidgetFieldSelectView($data['fields']['hour_format'])
+    )
+    ->addField(
+        new CWidgetFieldSelectView($data['fields']['color_scale_mode'])
+    )
+    ->addField(
+        new CWidgetFieldTextBoxView($data['fields']['color_scale_low'])
+    )
+    ->addField(
+        new CWidgetFieldTextBoxView($data['fields']['color_scale_high'])
+    )
+    ->addField(
+        new CWidgetFieldCheckBoxView($data['fields']['show_legend'])
+    )
+    ->addField(
+        new CWidgetFieldTextBoxView($data['fields']['legend_text'])
+    )
+    ->show();
